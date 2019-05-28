68a130be5e99efc52362a9f69e9fa1179f5001c6
Victor Lipovoy <vlipovoyemail@gmail.com>
2019-05-28 14:25:19 +0300

    Day 2

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,11 +1,73 @@
 import React, {Component} from 'react';
 import Post from './Post';
+import User from './User';
+import InstaService from '../services/instaservice';
+import ErrorMessage from './ErrorMessage';
 
 export default class Posts extends Component {
+  InstaService = new InstaService();
+  state = {
+    posts: [],
+    error: false
+  }
+
+  componentDidMount() {
+    this.updatePosts();
+  }
+
+  updatePosts() {
+    this.InstaService.getAllPosts()
+    .then(this.onPostsLoaded)
+    .catch(this.onError);
+  }
+
+  onPostsLoaded = (posts) => {
+    this.setState({
+      posts,
+      error: false
+    });
+  }
+
+  onError = (err) => {
+    this.setState({
+      error: true
+    })
+  }
+
+  renderItems(arr) {
+    return arr.map(item => {
+      const {name, altname, photo, src, alt, descr, id} = item;
+
+      return (
+        <div key={id} className="post">
+          <User
+            src={photo}
+            alt={altname}
+            name={name}
+            min/>
+          <img src={src} alt={alt}></img>
+          <div className="post__name">
+            {name}
+          </div>
+          <div className="post__descr">
+            {descr}
+          </div>
+        </div>
+      )
+    })
+  }
+
   render() {
+    const {error, posts} = this.state;
+    if (error) {
+      return <ErrorMessage />
+    }
+
+    const items = this.renderItems(posts);
+
     return (
       <div className="left">
-        <Post alt="" src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/42/Shaqi_jrvej.jpg/1200px-Shaqi_jrvej.jpg" />
+        {items}
       </div>
     )
   }
